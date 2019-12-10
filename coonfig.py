90685d9b4d8d17785e64dc4d7576e41079442618
zhili <1059489235@qq.com>
2019-12-10 18:25:03 +0800

    006

--- coonfig.py
+++ coonfig.py
@@ -10,4 +10,4 @@ os.path.dirname() --> 截取目录部分,获得的刚好是项目目录
 # 将项目路径保存给一个变量 --> 外面使用导包使用
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 print(BASE_DIR)
-# print(os.path.abspath(__file__))
+print(os.path.abspath(__file__))
